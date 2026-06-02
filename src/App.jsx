124d1260f8af8935fffbaea74f852b088f50cb4f
josemcnieto <josemcnieto@gmail.com>
2026-06-02 19:31:12 +0200

    segundo push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
-
+//componente principal: App
+//1.Importamos la hoja de estilos App.css
+import './App.css';
+//2. Importamos las dos grandes mitades de nuestra pantalla
+import sidebar from './Sidebar';
+importchatArea from './ChatArea';
 function App(){
   return(
-    <div>
-      <h1>¡Hola React! Nuestro entorno esta listo.🚀</h1>
+    //contenedor principal que usa flexbox(definido en el App.css)
+    //para poner las cosas lado a lado
+    <div className='app-container'>
+      {/*Inyectamos la mitad izquierda a la pantalla*/}
+      <sidebar />
+      {/* Inyectamos la mitad derecha de la pantalla*/}
+      <ChatArea />
+      
     </div>
   )
 }
